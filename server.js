4891f65b2ce8a0f2d37749e2924a509387b239a1
DantheDeveleoper <119366607+DantheDeveleoper@users.noreply.github.com>
2023-07-31 20:28:56 -0700

    Trying something

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const {Sequelize} = require('sequelize')
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize({
+    storage: process.env.PG_URI,
+    dialect: 'postgres',
+    username: 'postgres',
+    password: '1Mississippi\*'
+  })
+  
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
 
 // ROOT
 app.get('/', (req, res) => {
